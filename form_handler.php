824e207a6d021497bc97ad3cda458f6972986446
HiteshKumarJain <hiteshkumarjainhkj@gmail.com>
2023-08-18 16:13:33 +0530

    Initial commit

---
+++ form_handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$number = $_post['number'];
+$message = $_post['message'];
+
+$email_form = 'website emailid.com';
+
+$email_subject = 'New form submission';
+
+$email_body = "User Name: $name.\n ";
+                "User Email: $visitor_email.\n";
+                "number: $number.\n";
+                "User message: $message.\n";
+
+$to = 'bemdev5@gmail.com';
+
+$headers = "from: $email_form \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers)
+
+header("location: contact.html")
+?>
---
+++ script.js
@@ -0,0 +1,53 @@
+const counters = [
+    { id: 'project-counter', targetValue: 10 },
+    { id: 'land-counter', targetValue: 5 },
+    { id: 'years-counter', targetValue: 20 }
+];
+
+const duration = 1000; // Duration in milliseconds
+
+function animateCounter(counter) {
+    const counterElement = document.getElementById(counter.id);
+    const start = 0;
+    const end = counter.targetValue;
+
+    function updateCount(currentCount) {
+        counterElement.textContent = currentCount;
+    }
+
+    function animate(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+
+        const currentCount = Math.floor(progress * (end - start) + start);
+        updateCount(currentCount);
+
+        if (progress < 1) {
+            requestAnimationFrame(animate);
+        }
+    }
+
+    let startTime = null;
+    requestAnimationFrame(animate);
+}
+
+// Call the animation function when the element is in view
+function isInView(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+document.addEventListener('scroll', () => {
+    counters.forEach(counter => {
+        const counterElement = document.getElementById(counter.id);
+        if (isInView(counterElement)) {
+            animateCounter(counter);
+        }
+    });
+});
